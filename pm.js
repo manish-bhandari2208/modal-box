41f3d5e04ad1780d6f3955f20732b004502ee5d9
manish-bhandari2208 <162275037+manish-bhandari2208@users.noreply.github.com>
2024-03-17 15:28:52 +0530

    Add files via upload

---
+++ pm.js
@@ -0,0 +1,9 @@
+document.getElementById('contact').addEventListener('click',
+function(){
+document.querySelector('.bg-modal').style.display='flex';
+});
+
+document.querySelector('.close').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display='none';
+})
